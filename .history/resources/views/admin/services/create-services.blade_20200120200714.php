748a96fe0e2496cdf08e629a0818c3497a654502
aakashrajput <aakashsingh1999@gmail.com>
2020-01-20 20:46:22 +0530

    ui fix

--- .history/resources/views/admin/services/create-services.blade_20200120200714.php
+++ .history/resources/views/admin/services/create-services.blade_20200120200714.php
@@ -5,22 +5,21 @@
     @include('admin.layouts.customtopmenu')
     <!-- Header -->
     <!-- Header -->
-
-    @include('admin.layouts.customtopBar')
     <!-- Page content -->
     <div class="container-fluid mt--6">
       <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
           <div class="card-wrapper">
             <!-- Input groups -->
             <div class="card">
               <!-- Card header -->
               <div class="card-header">
-                <h3 class="mb-0">Edit Services SubCategory</h3>
+                <h3 class="mb-0">Create Service Page</h3>
               </div>
               <!-- Card body -->
               <div class="card-body">
-              <form action="/update-subcategory/{{$subcategory[0]->id}}" method="POST">
+                <form action="/save-services" method="POST">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
@@ -36,28 +35,46 @@
                           <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-user"></i></span>
                           </div>
-                        <input name="subcategoryname" class="form-control" placeholder="Category Name" value="{{$subcategory[0]->subcategoryname}}" type="text">
+                          <input name="servicename" class="form-control" placeholder="Service Name" type="text">
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div class="row">
                     <div class="col-md-6">
                       <div class="form-group">
                         <div class="input-group input-group-merge">
-                          <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-user"></i></span>
-                          </div>
-                          <select class="form-control" placeholder="Status" name="status">
-                            <option value="{{$subcategory[0]->status}}">{{ucfirst($subcategory[0]->status)}}(Current Status)</option>
+                          <select class="form-control" name="status" data-toggle="select" type="text">
                             <option value="active">Active</option>
                             <option value="inactive">Inactive</option>
-                        </select>
+                          </select>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <button type="submit" class="btn btn-success">Update SubCategory</button>
+                  <div class="card">
+                    <!-- Card header -->
+                    <div class="card-header">
+                      <h3 class="mb-0">Select Sub Category</h3>
+                    </div>
+                    <!-- Card body -->
+                    <div class="card-body">
+                        <select class="form-control" name="subcategory" data-toggle="select">
+                          @foreach ($subcategory as $subcategory)
+                          <option value="{{$subcategory->subcategoryname}}">{{ucfirst($subcategory->subcategoryname)}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                  </div>
+                  <div class="card">
+                    <!-- Card header -->
+                    <div class="card-header">
+                      <h3 class="mb-0">Add Service Page Content</h3>
+                    </div>
+                    <!-- Card body -->
+                    <div class="card-body">
+                      <textarea id="froala-editor" name="content">Initialize the Froala WYSIWYG HTML Editor on a textarea.</textarea>
+                    </div>
+                  </div>
+                  <button type="submit" class="btn btn-success">Submit</button>
                 </form>
               </div>
             </div>
