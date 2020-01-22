1749e564e4eb53619d339e1afce3d0c83f8bf7b4
aakashrajput <aakashsingh1999@gmail.com>
2020-01-23 02:14:16 +0530

    admin dashboard complete

---
+++ .history/resources/views/admin/services/addrating.blade_20200123015446.php
@@ -0,0 +1,75 @@
+@include('admin.layouts.header')
+
+<div class="main-content" id="panel">
+   <!-- Topnav -->
+   @include('admin.layouts.customtopmenu')
+   <!-- Header -->
+   <!-- Header -->
+
+   @include('admin.layouts.customtopBar')
+    <!-- Page content -->
+    <div class="container-fluid mt--6">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="card-wrapper">
+            <!-- Input groups -->
+            <div class="card">
+              <!-- Card header -->
+              <div class="card-header">
+                <h3 class="mb-0">Give Rating to Associate</h3>
+              </div>
+              <!-- Card body -->
+              <div class="card-body">
+              <form action="/admin/rate/{{$category[0]->id}}" method="POST">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    {{ csrf_field() }}
+                  <!-- Input groups with icon -->
+                  
+                  
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <div class="input-group input-group-merge">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                          </div>
+                          <select class="form-control" placeholder="Select Associate" name="name">
+                              @foreach($associate as $a)
+                            <option value="{{$a->name}}">{{ucfirst($a->name)}}</option>
+                            @endforeach
+                        </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <div class="input-group input-group-merge">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                          </div>
+                          <p>Give Rating out of 5</p>
+                          <select class="form-control" placeholder="Give Rating" name="associate_rating">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button type="submit" class="btn btn-success">Add Rating</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      @include('admin.layouts.footer')
---
+++ .history/resources/views/admin/services/rating.blade_20200123013433.php
@@ -0,0 +1,77 @@
+@include('.admin.layouts.header')
+   <!-- Main content -->
+   <div class="main-content" id="panel">
+   <!-- Topnav -->
+   @include('admin.layouts.customtopmenu')
+   <!-- Header -->
+   <!-- Header -->
+
+   @include('admin.layouts.customtopBar')
+     <!-- Page content -->
+     <div class="container-fluid mt--6">
+       <!-- Table -->
+       <div class="row">
+         <div class="col">
+           <div class="card">
+             <!-- Card header -->
+             <div class="card-header">
+               <h3 class="mb-0">Give Rating</h3>
+               <p class="text-sm mb-0">
+                See all the Associate Rating
+               </p>
+               <br>
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+                @endif
+             </div>
+             <div class="table-responsive py-4">
+               <table class="table table-flush" id="datatable-basic">
+                 <thead class="thead-light">
+                   <tr>
+                    <th>ID</th>
+                    <th>Associate</th>
+                    <th>Rating</th>
+                    <th>Give Rating</th>
+                   </tr>
+                 </thead>
+                 <tfoot>
+                    <tr>
+                        <th>ID</th>
+                        <th>Associate</th>
+                        <th>Rating</th>
+                        <th>Give Rating</th>
+                       </tr>
+                 </tfoot>
+                 <tbody>
+                     @foreach ($associate as $a)
+                    <tr>
+                        <td> {{ $a->id }} </td> 
+                        <td>{{ $a->name }}</td>
+                        <td>
+                            <div class="col-sm-4">
+                                This Associated is rated:
+                                <br>
+                                @for($i = 0; $i<$a->associate_rating; $i++)
+                                <i class="fa fa-sm fa-star"></i>
+                                @endfor
+                                {{$a->associate_rating}}/5
+                            </div>
+                        </td>
+                        <td>
+                            <a href="/admin/rating/{{ $a->id }}" type="button" class="btn btn-slack btn-icon-only rounded-circle">
+                                    <span class="btn-inner--icon"><i class="ni ni-settings-gear-65"></i></span>
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                 </tbody>
+               </table>
+             </div>
+           </div>
+         </div>
+       </div>
+       
+
+      @include('admin.layouts.footer')
